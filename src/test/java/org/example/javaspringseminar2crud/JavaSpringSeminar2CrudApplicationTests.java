9cec22e40eaf6817389d8100f9c16d3e2a817a24
ramilhagverdiyev <ramil.ax2503@gmail.com>
2024-04-05 10:42:14 +0400

    Initial commit

---
+++ src/test/java/org/example/javaspringseminar2crud/JavaSpringSeminar2CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaspringseminar2crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringSeminar2CrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
